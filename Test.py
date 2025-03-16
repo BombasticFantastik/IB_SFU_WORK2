aa4f768027292f3055255b48e72af604afcb3613
BombasticFantastik <artemilusenko2@gmail.com>
2025-03-16 18:40:19 +0700

    ДАААААААА ОНО РАБОТАЕТ, осталось только делёжку на слова сделать

--- Test.py
+++ Test.py
@@ -1,13 +1,10 @@
+d=5912021149162244363755593172
+s=4833238097010702717751415
 from random import randint
-from sympy import randprime
-
-def give_me_fucking_number():
-    N=0
-    while len(str(N))!=28:
-        P=randprime(2,(10**randint(2,28)-1))
-        Q=randprime(2,(10**randint(2,28)-1))
-        N=P*Q
-    return N,P,Q
-
-
-print(give_me_fucking_number())
+def find_e(d,s):
+    e=0
+    while (e*s)%d!=1:
+        e=randint(2,d)
+    return e
+        
+print(find_e(d,s))
